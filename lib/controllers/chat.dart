6e17a420e7774d7af45bf1949579ed265419fa8d
iivgll4 <gladkott@gmail.com>
2024-03-09 23:46:30 +0700

    + clean project

--- lib/controllers/chat.dart
+++ lib/controllers/chat.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ChatController extends GetxController {
   var messages = <Map<bool, String>>[].obs;
-  var isLoading = false.obs; // Флаг загрузки
+  var isLoading = false.obs;
 
   Future<void> fetchGeneratedText(String text) async {
-    isLoading.value = true; // Включаем индикатор загрузки
+    isLoading.value = true;
     final uri = Uri.parse('http://127.0.0.1:8080/generate');
     final headers = {'Content-Type': 'application/json'};
     final body = json.encode({'text': text});
@@ -19,16 +20,20 @@ class ChatController extends GetxController {
         final jsonResult = json.decode(httpResponse.body);
         messages.add({false: jsonResult['generated_text']});
       } else {
-        print('Server error: ${httpResponse.body}');
+        if (kDebugMode) {
+          print('Server error: ${httpResponse.body}');
+        }
       }
     } catch (e) {
-      print('HTTP request failed: $e');
+      if (kDebugMode) {
+        print('HTTP request failed: $e');
+      }
     } finally {
-      isLoading.value = false; // Выключаем индикатор загрузки
+      isLoading.value = false;
     }
   }
 
   void addMessage(String message) {
-    messages.add({true: message}); // Добавляем сообщение пользователя сразу
+    messages.add({true: message});
   }
 }
